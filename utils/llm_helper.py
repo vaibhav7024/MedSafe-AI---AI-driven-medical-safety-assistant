10b2a0b4fe849a79b79c22c3e8b8f80b8685a84f
vaibhav7024 <vjaiswal7024@gmail.com>
2026-06-29 20:40:48 +0530

    just refining the code

--- utils/llm_helper.py
+++ utils/llm_helper.py
@@ -10,6 +10,7 @@ api_key = os.getenv("GEMINI_API_KEY")
 if api_key:
     genai.configure(api_key=api_key)
 
+
 # We use gemini-2.5-flash for fast text generation
 generation_model = genai.GenerativeModel('gemini-2.5-flash')
 
